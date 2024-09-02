59a18332d067010fd19b54030e5a47a886c5ef43
jackamoss2 <112007713+jackamoss2@users.noreply.github.com>
2024-09-02 12:19:36 -0700

    lighting

--- main.js
+++ main.js
@@ -17,24 +17,48 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setAnimationLoop( animate );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 2, 2, 2 );
-const material = new THREE.MeshBasicMaterial( {
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshStandardMaterial( {
     color: 0x00ff00,
-    wireframe: true
+    // wireframe: true
 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
-camera.position.z = 5;
+
+// lighting
+scene.add(new THREE.AmbientLight(0xffffff, 0.5));
+const dirLight = new THREE.DirectionalLight(0xbfb58f, 1.0);
+dirLight.position.x += 50
+dirLight.position.y += 20
+dirLight.position.z += 40
+dirLight.castShadow = true
+dirLight.shadow.mapSize.width = 4096;
+dirLight.shadow.mapSize.height = 4096;
+const d = 25;
+dirLight.shadow.camera.left = - d;
+dirLight.shadow.camera.right = d;
+dirLight.shadow.camera.top = d;
+dirLight.shadow.camera.bottom = - d;
+
+let target = new THREE.Object3D();
+target.position.z = -30;
+dirLight.target = target;
+dirLight.target.updateMatrixWorld();
+
+dirLight.shadow.camera.lookAt(0, 0, -30);
+scene.add(dirLight);
+
 
 function animate() {
-
 	renderer.render( scene, camera );
-
 }
 
 
 // controls setup
+camera.position.x = -3;
+camera.position.y = 5;
+camera.position.z = 5;
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.target = new THREE.Vector3(0, 0, 0);
 controls.update();
