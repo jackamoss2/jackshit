0222c4b1a418323c80ac754604230e55942310bb
Jack Amoss <jackamoss96@gmail.com>
2024-09-07 09:25:50 -0700

    testing

--- geometry/convertXMLSurface.js
+++ geometry/convertXMLSurface.js
@@ -2,11 +2,18 @@ var surface = {};
 
 const domain = 'jackamoss2.github.io'
 
-fetch('https://' + domain + '/geometry/2_Faces.xml')
+// fetch('https://' + domain + '/geometry/2_Faces.xml')
+//   .then(response => response.text())
+//   .then((data) => {
+//     surface = data;
+//   })
+
+fetch('https://' + domain + '/geometry/text.txt')
   .then(response => response.text())
-  .then((data) => {
+  .then ((data) => {
     surface = data;
   })
+
 
 export { surface };
 
