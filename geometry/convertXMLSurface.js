2b0df4a861a523b1cd77f2611c25f3599c1ebf0b
Jack Amoss <jackamoss96@gmail.com>
2024-09-07 09:17:06 -0700

    cors test

--- geometry/convertXMLSurface.js
+++ geometry/convertXMLSurface.js
@@ -2,7 +2,7 @@ var surface = {};
 
 const domain = 'jackamoss2.github.io'
 
-fetch('http://' + domain + '/geometry/2_Faces.xml')
+fetch('https://' + domain + '/geometry/2_Faces.xml')
   .then(response => response.text())
   .then((data) => {
     surface = data;
