d944eac94e2db29a201afe2b066ca751a724a31d
Jack Amoss <jackamoss96@gmail.com>
2024-09-08 12:40:30 -0700

    goober page

--- geometry/convertXMLSurface.js
+++ geometry/convertXMLSurface.js
@@ -1,19 +1,2 @@
-var surface = {};
-
 const domain = 'jackamoss2.github.io'
-
-// fetch('https://' + domain + '/geometry/2_Faces.xml')
-//   .then(response => response.text())
-//   .then((data) => {
-//     surface = data;
-//   })
-
-fetch('https://' + domain + '/geometry/text.txt')
-  .then(response => response.text())
-  .then ((data) => {
-    surface = data;
-  })
-
-
-export { surface };
 
